004515f2acc32180068d8d718d3305b961f10238
alec <aleclogronio@gmail.com>
2022-11-16 19:52:52 +0800

    deployment

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import {useState} from 'react'
+import AddTaskForm from './components/AddTaskForm'
+import UpdateForm from './components/UpdateForm'
+import ToDo from './components/ToDo'  
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+function App() {
+  const [toDo, setToDo] = useState([])
+  const [newTask, setNewTask] = useState('')
+  const [updateData, setUpdateData] = useState('')
+  const addTask = () => {
+    if (newTask) {
+      let num = toDo.length + 1
+      let newEntry = {id: num, title: newTask, status: false }
+      setToDo([...toDo, newEntry])
+      setNewTask('')
+    } 
+  }  
+  const deleteTask = (id) => {
+    let newTasks = toDo.filter(task => task.id !== id)
+    setToDo(newTasks)
+
+  }
+
+  const markDone = (id) => {
+    let newTask = toDo.map(task =>
+      {
+        if (task.id === id ){
+          return ({...task,status: !task.status})
+          }
+          return task
+      })
+    setToDo(newTask)
+ }
+
+  const cancelUpdate = () => {
+    setUpdateData('')
+  }
+  const changeTask = (e) => {
+    let newEntry = {
+      id: updateData.id,
+      title: e.target.value,
+      status: updateData.status ? true : false
+    }
+    setUpdateData(newEntry)
+  }
+  const updateTask = () => {
+    let filterRecords = [...toDo].filter( task=>task.id !== updateData.id)
+    let updatedObject = [...filterRecords, updateData]
+    setToDo(updatedObject)
+    setUpdateData('')
+  }
+
+  return (
+    <div className="container App">
+      <br /><br />
+      <h2> ToDo List </h2>
+      <br /> <br />
+     
+      {updateData && updateData ? 
+      (      
+        <UpdateForm
+          updateData={updateData}
+          changeTask={changeTask}
+          updateTask={updateTask}
+          cancelUpdate={cancelUpdate}
+        />
+      ):( 
+        <AddTaskForm
+          newTask={newTask}
+          setNewTask={setNewTask}
+          addTask={addTask}
+        />
+      )}       
+      {toDo && toDo.length ? '' : <div className='gltch'>No Task...</div>}
+        <ToDo
+          toDo={toDo}
+          markDone={markDone} 
+          setUpdateData={setUpdateData} 
+          deleteTask={deleteTask}
+        />
+    
+    </div>
+  )
+}
+
+export default App
